25de82ec537109a56096fbecfc67999865743051
tangqi <1024505996@qq.com>
2017-10-14 22:18:06 +0800

    引入elementUI以及完成登录页面

---
+++ vuexdemo/src/components/login/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="login">
+      <div class="login-content">
+          <el-form :label-position="labelPosition" label-width="80px" :model="login">
+            <el-form-item label="账号">
+                <el-input v-model="login.name"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="login.password"></el-input>
+            </el-form-item>
+            <el-form-item>
+               <el-button type="primary" style="width: 100%">登录</el-button>
+            </el-form-item>
+        </el-form>   
+      </div>
+  </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                labelPosition: 'right',
+                login: {
+                    name: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+<style>
+ .login {
+     display: flex;
+     align-items: center;
+     justify-content: center;
+ }
+ .login-content {
+     padding:100px 0 0 0 ;
+     width: 400px;
+    
+
+ }
+
+</style>
--- vuexdemo/src/main.js
+++ vuexdemo/src/main.js
@@ -3,13 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+// vuex 
+import Vuex from 'vuex'
+import store from './store/index';
 
 Vue.config.productionTip = false
-
+//  第一步  ： 在mainjs 里面引入vuex 然后进行全局的注册
+// vuex 的入口 就是在store中的indexjs 里面 
+Vue.use(ElementUI);
+Vue.use(Vuex);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
